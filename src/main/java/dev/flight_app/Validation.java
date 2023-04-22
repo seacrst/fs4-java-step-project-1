59bea4e63089dfe8e38d989d4749d537021bf866
Oleksandr Kobets <cosmopolicy@gmail.com>
2023-04-22 18:45:28 +0300

    modified: FlightController.java FlightService.java Validation.java

--- src/main/java/dev/flight_app/Validation.java
+++ src/main/java/dev/flight_app/Validation.java
@@ -1,57 +1,65 @@
 package dev.flight_app;
 
+import dev.flight_app.controllers.FlightController;
 import dev.flight_app.entities.Airline;
+import dev.flight_app.entities.City;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.stream.Stream;
 
 public class Validation {
-    public boolean isValidName(String s){
+    public boolean isValidName(String s) {
         if (s.length() < 2) return false;
         return s.chars().allMatch(Character::isLetter);
     }
-    public static boolean isValidPassword(String s){
+
+    public static boolean isValidPassword(String s) {
         if (s.length() < 6) return false;
         return s.chars().anyMatch(Character::isDigit) && s.chars().anyMatch(Character::isLowerCase)
-                && s.chars().anyMatch(Character::isUpperCase) ;
+                && s.chars().anyMatch(Character::isUpperCase);
     }
-    public static boolean isValidLogin(String s){
+
+    public static boolean isValidLogin(String s) {
         if (s.length() > 2 && s.length() < 20) return false;
         return s.chars().anyMatch(Character::isLetter) && s.chars().anyMatch(Character::isLetterOrDigit);
     }
-    public static String Login(String s){
-        while(!isValidLogin(s)){
+
+    public static String Login(String s) {
+        while (!isValidLogin(s)) {
             Console.println("Try again");
             s = ScannerConstrained.nextLine();
         }
         return s;
     }
 
-    public boolean isValidPassengerQt(String s){
+    public boolean isValidPassengerQt(String s) {
         return s.chars().allMatch(c -> Character.isDigit(c) && c != '0');
     }
-    public static String Pass(String s){
-        while(!isValidPassword(s)){
+
+    public static String Pass(String s) {
+        while (!isValidPassword(s)) {
             Console.println("Try again");
             s = ScannerConstrained.nextLine();
         }
         return s;
     }
-    public boolean isValidId(String id){
+
+    public boolean isValidId(String id) {
         return id.chars().allMatch(Character::isDigit) && id.equals("0");
     }
 
-    public boolean isValidAirline(String s){
-        for(Airline airline: Airline.values()){
-            if(airline.toString().equals(s.toUpperCase())) return true;
+    public boolean isValidAirline(String s) {
+        for (Airline airline : Airline.values()) {
+            if (airline.toString().equals(s.toUpperCase())) return true;
         }
         return false;
     }
-    public boolean validateDate(String s){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public boolean validateDate(String s) {
         try {
-            LocalDate date = LocalDate.parse(s, formatter);
+            LocalDate date = LocalDate.parse(s, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
             return true;
         } catch (DateTimeParseException ex) {
             System.out.println("Incorrect date format. The required format is \"dd-MM-yyyy\"");
@@ -59,9 +67,60 @@ public class Validation {
         }
     }
 
-    public static LocalDate stringToDate (String s) {
-            return LocalDate.parse(s);
+    public static boolean validateCity(String s) {
+        s = stringNormalisation(s);
+        try {
+            City city = City.valueOf(s);
+            return true;
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Incorrect city format or this city could not be found in the database.");
+            return false;
+        }
     }
 
+    public static String stringNormalisation(String s) {
+        StringBuilder sb = new StringBuilder();
+        s.toUpperCase()
+                .codePoints()
+                .filter(x -> x >= 65 && x <= 90 || x == 95)
+                .mapToObj(x -> (char) x)
+                .forEach(sb::append);
+        return sb.toString();
+    }
 
+    public static boolean validateFlightId(String s) {
+        s = stringNormalisation(s);
+        try {
+            int id = Integer.parseInt(s);
+            return id > 0 && id <= 500;
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Incorrect Flight ID format or this ID could not be found in the database.");
+            return false;
+        }
+    }
+    public static boolean validateSeatsQuantity(String s) {
+        s = stringNormalisation(s);
+        try {
+            int seatsQuantity = Integer.parseInt(s);
+            return seatsQuantity >= 0 && seatsQuantity <= 400;
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Incorrect number of seats.");
+            return false;
+        }
+    }
+
+    public static LocalDate stringToDate(String s) {
+        return LocalDate.parse(s, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
+    public static City stringToCity(String s) {
+        s = stringNormalisation(s);
+        return City.valueOf(s);
+    }
+    public static int stringToFlightId(String s) {
+        return Integer.parseInt(s);
+    }
+    public static int stringToSeatsQuantity(String s) {
+        return Integer.parseInt(s);
+    }
 }
--- src/main/java/dev/flight_app/controllers/FlightController.java
+++ src/main/java/dev/flight_app/controllers/FlightController.java
@@ -1,10 +1,13 @@
 package dev.flight_app.controllers;
 
+import dev.flight_app.Validation;
+import dev.flight_app.entities.City;
 import dev.flight_app.entities.Console;
 import dev.flight_app.dao.FlightDao;
 import dev.flight_app.entities.Flight;
 import dev.flight_app.services.FlightService;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,20 +28,18 @@ public class FlightController {
 //    }
 
     private FlightController() {
-
     }
 
     public static FlightController create() {
         if (!Objects.isNull(fc)) {
             return fc;
         }
-
         fc = new FlightController();
-
         return fc;
     }
 
-    public List<Flight> selectById(int id) {
+    public List<Flight> selectById(String idString) {
+        int id = Validation.stringToFlightId(idString);
         return flightService.selectById(id);
     }
 
@@ -46,21 +47,35 @@ public class FlightController {
         return flightService.selectByFlightCode(flightCode);
     }
 
+    public List<Flight> selectByCityDateSeats(String cityString, String dateString, String seatsString) {
+        City city = Validation.stringToCity(cityString);
+        LocalDate date = Validation.stringToDate(dateString);
+        int seatsQuantity = Validation.stringToSeatsQuantity(seatsString);
+        List<Flight> byCityDeparture = flightService.selectByDepartureCity(City.KYIV);
+        List<Flight> plusByCityArrival = flightService.selectByArrivalCity(byCityDeparture, city);
+        List<Flight> plusByDate = flightService.selectByDepartureDate(plusByCityArrival, date);
+        List<Flight> plusBySeatsQuantity = flightService.selectBySeatsQuantity(plusByDate, seatsQuantity);
+        return plusBySeatsQuantity;
+    }
+
+    public List<Flight> selectByDepartureCity(String cityString) {
+        City city = Validation.stringToCity(cityString);
+        return flightService.selectByDepartureCity(city);
+    }
+
+    public List<Flight> selectByDepartureDate(String dateString) {
+        LocalDate date = Validation.stringToDate(dateString);
+        return flightService.selectByDepartureDate(date);
+    }
+
+    public List<Flight> selectBySeatsQuantity(String seatsString) {
+        int seatsQuantity = Validation.stringToSeatsQuantity(seatsString);
+        return flightService.selectBySeatsQuantity(seatsQuantity);
+    }
 
     public List<Flight> allFlights() {
         return flightService.getAll();
     }
-
-//    public void displayAllFlights() {
-//        allFlights().forEach(Console::output);
-//    }
-//
-//    public void displayFlights(List<Flight> flights) {
-//        flights.forEach(Console::output);
-//    }
-
-
-
 
     public void loadData() {
         flightService.loadData();
