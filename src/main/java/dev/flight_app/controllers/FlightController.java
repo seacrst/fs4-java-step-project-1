06b6ba2536abc59dfa77468b3047701e909e8fb6
Oleksandr Kobets <cosmopolicy@gmail.com>
2023-04-22 11:39:18 +0300

    displayAllFlights

--- src/main/java/dev/flight_app/controllers/FlightController.java
+++ src/main/java/dev/flight_app/controllers/FlightController.java
@@ -1,12 +1,23 @@
 package dev.flight_app.controllers;
 
+import dev.flight_app.entities.Flight;
 import dev.flight_app.services.FlightService;
+
+import java.util.List;
 
 public class FlightController {
     private final FlightService flightService;
 
     public FlightController(FlightService flightService) {
         this.flightService = flightService;
+    }
+
+    public List<Flight> allFlights(){
+        return flightService.getAll();
+    }
+
+    public void displayAllFlights() {
+        System.out.println(allFlights());
     }
 
     // метод принимает место отправления, место назначения, дата вылета, количество человек (сколько необходимо купить билетов). Возвращает список рейсов удовлетворяющих условиям.
